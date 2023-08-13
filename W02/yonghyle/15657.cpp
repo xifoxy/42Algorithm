8823f7e1cad5679ff42707330ab1b63d34fe79b8
YongHyun-Yi <dldydgus1995@naver.com>
2023-08-13 09:59:03 +0900

    save

---
+++ W02/yonghyle/15657.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+int input[8];
+vector<int> num;
+
+void recur(int idx, int depth)
+{
+	while (1)
+	{
+		if (depth == m)
+		{
+			for (int j : num)
+				cout << j << " ";
+			cout << "\n";
+			return ;
+		}
+		else if (idx >= n)
+			return ;
+		num.push_back(input[idx]);
+		recur(idx, depth + 1);
+		num.pop_back();
+		idx++;
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	memset(input, 9, sizeof(int) * 8);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> input[i];
+	sort(input, input + 8);
+
+	recur(0, 0);
+}
