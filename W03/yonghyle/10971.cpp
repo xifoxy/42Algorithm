10a92e46c10869f2516668e572513e25b668d09a
YongHyun-Yi <dldydgus1995@naver.com>
2023-09-20 11:35:19 +0900

    최적화된 외판원 실패...

--- W03/yonghyle/10971.cpp
+++ W03/yonghyle/10971.cpp
@@ -7,31 +7,29 @@ using namespace std;
 int n;
 long long result = -1;
 int arr[10][10];
-
-vector<int> visit_record;
+bool visit[10];
 
 void  rec(int depth, int city, long long sum)
 {
-	if (city >= n)
-		return ;
-
-	if  (depth == n - 1 && arr[city][visit_record[0]] != 0)
+	if  (depth == n - 1)
 	{
-		if (result == -1 || sum + arr[city][visit_record[0]] < result)
-			result = sum + arr[city][visit_record[0]];
+		if (arr[city][0] != 0)
+		{
+			if (result == -1 || sum + arr[city][0] < result)
+				result = sum + arr[city][0];
+		}
 		return ;
 	}
 
-	visit_record.push_back(city);
 	for (int i = 0; i < n; i++)
 	{
-		if (find(visit_record.begin(), visit_record.end(), i) == visit_record.end() && arr[city][i] != 0) // vector에 없고 갈 수 있는 길이 있을 때
-			rec(depth + 1, i, sum + arr[city][i]);
+		if (visit[i] == true || arr[city][i] == 0)
+			continue;
+		
+		visit[i] = true;
+		rec(depth + 1, i, sum + arr[city][i]);
+		visit[i] = false;
 	}
-	visit_record.erase(find(visit_record.begin(), visit_record.end(), city));
-
-	if (depth == 0 && city < n)
-		rec(0, city + 1, 0);
 }
 
 int main()
