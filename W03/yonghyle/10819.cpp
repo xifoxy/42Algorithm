8dfe6c68756ca980b3ee870735745ef9190f0f0e
YongHyun-Yi <dldydgus1995@naver.com>
2023-09-22 11:02:32 +0900

    차이를 최대로 클리어

--- W03/yonghyle/10819.cpp
+++ W03/yonghyle/10819.cpp
@@ -6,68 +6,28 @@ int n, result;
 int input[8];
 bool visit[8];
 
-void rec(int depth, int cur_num, int prev_num, int sum)
+void rec(int depth, int prev_idx, int sum)
 {
-	cout << "depth: " << depth << "\n";
-	cout << "prev_num: " << input[prev_num] << "\n";
-	cout << "cur_num: " << input[cur_num] << "\n";
-	cout << "selected: ";
-	for (int i = 0; i < n; i++)
-		if (visit[i] == true)
-			cout << input[i] << " ";
-	cout << "\n";
-	cout << "sum: " << sum << "\n";
-	cout << "-------------------------\n";
-
 	if (depth == n)
 	{
-		// sum += abs(input[prev_num] - input[cur_num]);
-		// cout << "depth: " << depth << "\ncur_num: " << cur_num << "\nprev_num: " << prev_num << "\nsum: " << sum << "\n";
-		
-		if (sum > result)
+		if (result < sum)
 			result = sum;
 		return;
 	}
 
-	
 	for (int i = 0; i < n; i++)
 	{
 		if (visit[i] == true)
 			continue;
 		
-		visit[i] = true;	
+		visit[i] = true;
 		if (depth == 0)
-			rec(depth + 1, i, cur_num, sum);
+			rec(depth + 1, i, 0);
 		else
-			rec(depth + 1, i, cur_num, sum + abs(input[prev_num] - input[cur_num]));
+			rec(depth + 1, i, sum + abs(input[prev_idx] - input[i]));
 		visit[i] = false;
 	}
 }
-
-// void rec(int depth, int sum)
-// {
-// 	cout << "depth: " << depth << "\n";
-// 	cout << "selected: ";
-// 	for (int i = 0; i < n; i++)
-// 		if (visit[i] == true)
-// 			cout << i << " ";
-// 	cout << "\n";
-// 	cout << "sum: " << sum << "\n";
-// 	cout << "-------------------------\n";
-
-// 	if (depth == n)
-// 		return;
-
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		if (visit[i])
-// 			continue;
-		
-// 		visit[i] = true;
-// 		rec(depth + 1, sum + i);
-// 		visit[i] = false;
-// 	}
-// }
 
 int main()
 {
@@ -78,7 +38,6 @@ int main()
 	for (int i = 0; i < n; i++)
 		cin >> input[i];
 	
-	rec(0, 0, 0, 0);
-	// rec(0, 0);
+	rec(0, 0, 0);
 	cout << result;
 }
