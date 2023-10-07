46a3689f03a3bf77ac1f38d8318332eb621e9b99
YongHyun-Yi <dldydgus1995@naver.com>
2023-10-07 22:29:07 +0900

    아................

--- W04/yonghyle/1010.cpp
+++ W04/yonghyle/1010.cpp
@@ -2,21 +2,42 @@
 
 using namespace std;
 
-int n, m, arr[30][30];
+int n, m, arr[31][31];
 
 int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 
-	for (int i = 0; i < 30; i++)
-		arr[i][i] = 1;
+	// for (int i = 1; i <= 30; i++)
+	// 	arr[1][i] = i;
+	
+	// for (int i = 2; i <= 30; i++)
+	// {
+	// 	for (int j = i + 1; j <= 30; j++)
+	// 	{
+	// 		arr[i][j] = 
+	// 	}
+	// }
 
-	arr[0][0] = 1;
-	for (int i = 0; i < 30; i++)
+	// for (int i = 1; i <= 30; i++)
+		// arr[i][i] = 1;
+	
+	for (int i = 1; i <= 30; i++)
+		arr[1][i] = i;
+	
+	for (int i = 2; i <= 30; i++)
 	{
-		for (int j = i + 1; j < 30; j++)
-			arr[i][j] = arr[i][j - 1] + (i + 1);
+		for (int j = i; j <= 30; j++)
+		{
+			for (int x = i - 1; x > 0; x--)
+			{
+				for (int y = j - 1; y > 0; y--)
+				{
+					//
+				}
+			}
+		}
 	}
 
 	int t;
@@ -25,6 +46,13 @@ int main()
 	{
 		
 		cin >> n >> m;	
-		cout << arr[n - 1][m - 1] << "\n";
+		cout << arr[n][m] << "\n";
+	}
+
+	for (int i = 1; i <= 30; i++)
+	{
+		for (int j = 1; j <= 30; j++)
+			cout << arr[i][j] << " ";
+		cout << "\n";
 	}
 }
