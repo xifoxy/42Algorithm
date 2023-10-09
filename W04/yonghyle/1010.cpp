87feb64b345537cdabaf4132a6610bd602015bc0
YongHyun-Yi <dldydgus1995@naver.com>
2023-10-10 02:18:13 +0900

    두 가지 방법으로 모두 통과, 점화식은 여러개가 나올 수 있다, 메모이제이션은 메모하는 기술

--- W04/yonghyle/1010.cpp
+++ W04/yonghyle/1010.cpp
@@ -8,37 +8,30 @@ int main()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-
-	// for (int i = 1; i <= 30; i++)
-	// 	arr[1][i] = i;
-	
-	// for (int i = 2; i <= 30; i++)
-	// {
-	// 	for (int j = i + 1; j <= 30; j++)
-	// 	{
-	// 		arr[i][j] = 
-	// 	}
-	// }
-
-	// for (int i = 1; i <= 30; i++)
-		// arr[i][i] = 1;
 	
 	for (int i = 1; i <= 30; i++)
 		arr[1][i] = i;
 	
+	for (int i = 1; i <= 30; i++)
+		arr[i][i] = 1;
+	
 	for (int i = 2; i <= 30; i++)
 	{
-		for (int j = i; j <= 30; j++)
+		for (int j = i + 1; j <= 30; j++)
 		{
-			for (int x = i - 1; x > 0; x--)
-			{
-				for (int y = j - 1; y > 0; y--)
-				{
-					//
-				}
-			}
+			for (int y = j - 1; y >= i - 1; y--)
+				arr[i][j] += arr[i - 1][y];
 		}
 	}
+
+	// for (int i = 0; i <= 30; i++)
+	// 	arr[0][i] = 1;
+	
+	// for (int i = 1; i <= 30; i++)
+	// {
+	// 	for (int j = 1; j <= 30; j++)
+	// 		arr[i][j] = arr[i - 1][j - 1] + arr[i][j - 1];
+	// }
 
 	int t;
 	cin >> t;
@@ -47,12 +40,5 @@ int main()
 		
 		cin >> n >> m;	
 		cout << arr[n][m] << "\n";
-	}
-
-	for (int i = 1; i <= 30; i++)
-	{
-		for (int j = 1; j <= 30; j++)
-			cout << arr[i][j] << " ";
-		cout << "\n";
 	}
 }
