87b8e042d1e9a8aca1b0eb192b2efeb012c38b34
YongHyun-Yi <dldydgus1995@naver.com>
2023-11-01 19:53:36 +0900

    정답...다시 풀어보기...

--- W04/yonghyle/2156.cpp
+++ W04/yonghyle/2156.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int input[10001], score[10001][3], result = 0;
+long long input[10001], score[10001][3], result = 0;
 
 int main()
 {
@@ -16,9 +16,9 @@ int main()
 	score[1][1] = input[1];
 	score[2][1] = input[2];
 	score[2][2] = input[1] + input[2];
-	for (int i = 3; i <= n; i++)
+	for (int i = 3; i <= n + 2; i++) // - 3 에 맞춰서 범위 확장...어째서??
 	{
-		score[i][1] = max(score[i - 2][1], score[i - 2][2]) + input[i];
+		score[i][1] = max(max(score[i - 2][1], score[i - 2][2]), max(score[i - 3][1], score[i - 3][2])) + input[i];
 		score[i][2] = score[i - 1][1] + input[i];
 		result = max(result, max(score[i][1], score[i][2]));
 	}
